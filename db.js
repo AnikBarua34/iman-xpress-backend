56aec087948156f631e54eb88c345a9710f9f880
Tushar <freelancerrtushar@gmail.com>
2022-03-03 15:55:46 +0600

    some changes

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lp6z6.mongodb.net/imanXpress?retryWrites=true&w=majority`;
+
 
 const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lp6z6.mongodb.net/imanxpress?retryWrites=true&w=majority`;
 
@@ -8,6 +8,8 @@ const connectMongo = () => {
     mongoose.connect(mongoURI,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
        
        
     }).then(() => console.log("Database connected!"))
