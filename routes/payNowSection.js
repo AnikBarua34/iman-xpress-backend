44f8350e34643f0aa682a13b699aa8fb4ad1dfdb
Tushar <freelancerrtushar@gmail.com>
2022-03-13 17:03:15 +0600

    somechanges

--- routes/payNowSection.js
+++ routes/payNowSection.js
@@ -8,14 +8,14 @@ const order = require("../models/order")
 
 // payment initialization 
 router.post('/init', async (req, res) => {
-   console.log(req.body);
-    
+    console.log(req.body);
+
 
     const data = {
         tran_id: uuidv4(),
         merchant_id: req.body.merchant_id,
         total_amount: req.body.total_amount,
-        payment_status:'Pending',
+        payment_status: 'Pending',
         cus_name: req.body.cus_name,
         cus_email: req.body.cus_email,
         cus_phone: req.body.cus_phone,
@@ -24,11 +24,11 @@ router.post('/init', async (req, res) => {
         note: req.body.note,
         cus_postcode: req.body.cus_postcode,
         streetAddress: req.body.streetAddress,
-        currency: 'BDT', 
-        success_url: 'https://iman-xpress.herokuapp.com/api/payNow/success',
-        fail_url: 'https://iman-xpress.herokuapp.com/api/payNow/fail',
-        cancel_url: 'https://iman-xpress.herokuapp.com/api/payNow/cancel',
-        ipn_url: 'https://iman-xpress.herokuapp.com/api/payNow/ipn',
+        currency: 'BDT',
+        success_url: 'http://localhost:8080/api/payNow/success',
+        fail_url: 'http://localhost:8080/api/payNow/fail',
+        cancel_url: 'http://localhost:8080/api/payNow/cancel',
+        ipn_url: 'http://localhost:8080/api/payNow/ipn',
         shipping_method: 'Courier',
         product_name: 'none',
         product_image: 'none',
@@ -52,64 +52,64 @@ router.post('/init', async (req, res) => {
         value_c: 'none',
         value_d: 'none'
     };
-// insert data 
+    // insert data 
     const orderData = await order.create(data);
 
-const sslcommer = new SSLCommerzPayment(process.env.STORE_ID,process.env.STORE_PASS,false) //true for live default false for sandbox
-sslcommer.init(data).then(data => {
-   
- 
-    //process the response that got from sslcommerz 
-    //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-    if(data.GatewayPageURL){
-        res.json(data.GatewayPageURL)
-    }
-    else{
-        return res.status(400).json({
-            message: 'Payment session failed'
-        })
-    }
-});
-  
+    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASS, false) //true for live default false for sandbox
+    sslcommer.init(data).then(data => {
+
+
+        //process the response that got from sslcommerz 
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+        if (data.GatewayPageURL) {
+            res.json(data.GatewayPageURL)
+        }
+        else {
+            return res.status(400).json({
+                message: 'Payment session failed'
+            })
+        }
+    });
+
 })
-router.post('/success', async(req,res)=>{
-    const orders = await order.updateOne({tran_id: req.body.tran_id},{
-        $set:{
-            val_id:req.body.val_id
+router.post('/success', async (req, res) => {
+    const orders = await order.updateOne({ tran_id: req.body.tran_id }, {
+        $set: {
+            val_id: req.body.val_id
         }
     })
-   console.log(req.body.val_id);
-    res.status(200).redirect(`https://iman-xpress.netlify.app/success/${req.body.tran_id}`)
+    console.log(req.body.val_id);
+    res.status(200).redirect(`http://localhost:3000/success/${req.body.tran_id}`)
 })
-router.post('/fail', async(req,res)=>{
-   const orders = await order.deleteOne({tran_id:req.body.tran_id})
-    res.status(400).redirect(`https://iman-xpress.netlify.app/failed`)
+router.post('/fail', async (req, res) => {
+    const orders = await order.deleteOne({ tran_id: req.body.tran_id })
+    res.status(400).redirect(`http://localhost:3000/failed`)
 })
-router.post('/cancel', async(req,res)=>{
-    const orders = await order.deleteOne({tran_id:req.body.tran_id})
-    res.status(200).redirect(`https://iman-xpress.netlify.app/`)
+router.post('/cancel', async (req, res) => {
+    const orders = await order.deleteOne({ tran_id: req.body.tran_id })
+    res.status(200).redirect(`http://localhost:3000/`)
 })
-router.post('/validate', async (req,res)=>{
+router.post('/validate', async (req, res) => {
     console.log(req.body);
-    const orders = await order.findOne({tran_id: req.body.tran_id});
-    
-    if(orders.val_id === req.body.val_id){
-        const update = await order.updateOne({tran_id: req.body.tran_id},{
+    const orders = await order.findOne({ tran_id: req.body.tran_id });
+
+    if (orders.val_id === req.body.val_id) {
+        const update = await order.updateOne({ tran_id: req.body.tran_id }, {
             $set: {
                 payment_status: 'Success'
             }
         })
-        res.send(update.modifiedCount>0)
+        res.send(update.modifiedCount > 0)
     }
-    else{
+    else {
         res.send('Payment not confirmed. Order Discarded')
     }
 });
 
-router.get('/orders/:tran_id', async (req,res)=>{
+router.get('/orders/:tran_id', async (req, res) => {
     const id = req.params.tran_id;
-    const orders = await order.findOne({tran_id: id});
-   
+    const orders = await order.findOne({ tran_id: id });
+
     res.json(orders)
 })
 
