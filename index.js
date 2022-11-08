106bfeb45775e550b821239cb9e30071af088c8f
AnikBarua34 <anikbaruaturjoy34@gmail.com>
2022-11-08 18:10:31 +0600

    new push

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-require('dotenv').config()
-var cors = require('cors')
-const port =process.env.PORT || 8080 
+require("dotenv").config();
+var cors = require("cors");
+const port = process.env.PORT || 8080;
 
+// anik iman
+const authmerchantUser = require("./routes/auth");
+const authRiderUser = require("./routes/authRider");
+const riderProfile = require("./routes/riderProfile");
+const merchantProduct = require("./routes/merchantproduct");
+const blogsection = require("./routes/blogSection");
+const authgeneraluser = require("./routes/generalAuth");
+const partnerSection = require("./routes/partnerSection");
+const payNowSection = require("./routes/payNowSection");
 
-const authmerchantUser = require("./routes/auth")
-const authRiderUser = require("./routes/authRider")
-const riderProfile = require("./routes/riderProfile")
-const merchantProduct = require("./routes/merchantproduct")
-const blogsection = require("./routes/blogSection")
-const authgeneraluser = require("./routes/generalAuth")
-const partnerSection = require("./routes/partnerSection")
-const payNowSection = require("./routes/payNowSection")
-
-
-
-const connecttoMongo = require("./db")
+const connecttoMongo = require("./db");
 connecttoMongo();
-app.use(express.json())
-app.use(cors())
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 //Available routes
-app.use("/api/auth", authmerchantUser)
-app.use("/api/merchant", merchantProduct)
-app.use("/api/blog",blogsection)
-app.use("/api/authRider", authRiderUser)
-app.use("/api/authgeneral", authgeneraluser)
-app.use("/api/riderProfile", riderProfile)
-app.use("/api/partner", partnerSection)
-app.use("/api/payNow", payNowSection)
-
+app.use("/api/auth", authmerchantUser);
+app.use("/api/merchant", merchantProduct);
+app.use("/api/blog", blogsection);
+app.use("/api/authRider", authRiderUser);
+app.use("/api/authgeneral", authgeneraluser);
+app.use("/api/riderProfile", riderProfile);
+app.use("/api/partner", partnerSection);
+app.use("/api/payNow", payNowSection);
 
 app.get("/", (req, res) => {
-    res.json("IMANXpress Server")
+  res.json("IMANXpress Server");
 });
 
 app.listen(port, () => {
-    console.log(`localhost running in ${port}`)
-})
+  console.log(`localhost running in ${port}`);
+});
